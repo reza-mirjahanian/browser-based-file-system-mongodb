f85f713e6aeb4d8dad45f24fe060f9c5e6d255c7
Reza Mirjahanian <rmirjahanian@gmail.com>
2021-07-31 11:28:40 +0430

    constant

--- server/constants.js
+++ server/constants.js
@@ -7,10 +7,14 @@ const
 
 const constants = {
   EXPRESS_PORT: Number(process.env.PORT) || 3100,
+  FILE_TYPE: {
+    FILE: 'file',
+    FOLDER: 'folder'
+  },
   MONGODB: {
     connections: {
       default: {
-        uri: process.env.COFFEE_MONGO_DB_URI || localUri + `coffee${isTestMode ? '_test' : (isDevMode ? '_dev' : '')}`
+        uri: process.env.COFFEE_MONGO_DB_URI || localUri + `truebase${isTestMode ? '_test' : (isDevMode ? '_dev' : '')}`
       }
     }
   },
